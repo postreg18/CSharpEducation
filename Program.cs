5bce0d2f54fff7ade4b0688849fc562b4f6813d7
Karabas <postreg18@yandex.ru>
2025-05-03 17:47:59 +0400

    Объявите и инициализируйте переменную

--- Program.cs
+++ Program.cs
@@ -1,12 +1,16 @@
 ﻿using System;
 
-namespace Task1
+namespace Task2//C#. Структура программы, переменные
 {
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            int age = 33;//целочисленная переменная, обозначающая возраст
+            string name = "Boris";//строковая переменная для обозначения имени
+            string nameCompany;//строковая переменная для обозначения названия компании
+            bool isEmployed = false;//переменная логического типа
+            double weight = 78.65;//переменная типа с плавающей точкой для обозначения веса тела человека
         }
     }
 }
